fb731d2c1038041f69e5ded2822b72089163062d
unknown <amiradib483@gmail.com>
2023-08-01 18:24:34 +0330

    migrations

---
+++ database/migrations/2023_07_29_072828_create_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->string("slug")->unique()->nullable();
+            $table->text("summary");
+            $table->text("body");
+            $table->text("image");
+            $table->tinyInteger("status")->default(0);
+            $table->tinyInteger("commentable")->default(0)->comment("0 => UnCommentable, 1 => Commentable");
+            $table->string("tags");
+            $table->timestamp("published_at");
+            $table->foreignId("author_id")->constrained("users");
+            $table->foreignId("category_id")->constrained("post_categories");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
